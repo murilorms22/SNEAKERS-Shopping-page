40f2d9a27ae4385cdde821926976f6433f28eb71
Murilo <murilorms22@gmail.com>
2025-04-04 12:19:00 -0300

    progress bar, cardproduto arrumado

--- script.js
+++ script.js
@@ -3,11 +3,49 @@ function sleep(ms) {
 }
 
 async function loop() {
-    for(i=1; i<=3; i++){
-        console.log(i);
-        await sleep(1000);
+    
+    for(let i=1; i<=3; i++){
+
+        if(i == 1) {
+            document.getElementById("imgCarrossel").src = "assets/images/puma.jpg"
+            
+        }
+        
+        if(i == 2) {
+            document.getElementById("imgCarrossel").src = "assets/images/skate.jpg"
+        }
+        
+        if(i == 3) {
+            document.getElementById("imgCarrossel").src = "assets/images/luka.jpg"
+
+        }
+        
+        await sleep(10000);
         if(i==3) {
             i = 0;
         }
     }
+}
+
+function startProgressBar() {
+    const progress = document.getElementById("progress");
+
+    function loop() {
+        progress.style.transition = "none";
+        progress.style.width = "0%";
+
+        void progress.offsetWidth;
+
+        progress.style.transition = "width 10s linear";
+        progress.style.width = "100%";
+
+        setTimeout(loop, 10000);
+    }
+
+    loop();
+}
+
+function iniciarTudo() {
+    startProgressBar();
+    loop();
 }
